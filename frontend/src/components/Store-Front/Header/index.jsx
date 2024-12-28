db0626cfe7b274c8b688a9e2a8a60c7603c5f658
hnaqvi101 <haidernaqvi0007@gmail.com>
2024-12-29 00:35:28 +0500

    front page mobile view changes

--- frontend/src/components/Store-Front/Header/index.jsx
+++ frontend/src/components/Store-Front/Header/index.jsx
@@ -40,39 +40,38 @@ export const StoreFrontHeader = () => {
             aria-hidden="true"
             className="absolute inset-0 hidden sm:flex sm:flex-col"
           >
-            <div className="relative w-full flex-1 bg-gray-800">
+            <div className="relative flex-1 w-full bg-gray-800">
               <div className="absolute inset-0 overflow-hidden">
                 <img
                   src="/turkfy/mockup.jpg"
                   alt=""
-                  className="h-full w-full object-cover object-center"
+                  className="object-cover object-center w-full h-full"
                 />
               </div>
               <div className="absolute inset-0 bg-gray-900 opacity-50" />
             </div>
-            <div className="h-32 w-full bg-white md:h-40 lg:h-48" />
+            <div className="w-full h-32 bg-white md:h-40 lg:h-48" />
           </div>
 
-          <div className="relative mx-auto max-w-5xl px-4 pb-96 text-center sm:px-6 sm:pb-0 lg:px-8">
+          <div className="relative max-w-5xl px-4 mx-auto text-center pb-96 sm:px-6 sm:pb-0 lg:px-8">
             {/* Background image and overlap */}
             <div
               aria-hidden="true"
               className="absolute inset-0 flex flex-col sm:hidden"
             >
-              <div className="relative w-full flex-1 bg-gray-800">
+              <div className="relative flex-1 w-full bg-gray-800">
                 <div className="absolute inset-0 overflow-hidden">
                   <img
                     src="/turkfy/mockup.jpg"
                     alt=""
-                    className="h-full w-full object-cover object-center"
+                    className="object-cover object-center w-full h-full"
                   />
                 </div>
                 <div className="absolute inset-0 bg-gray-900 opacity-50" />
               </div>
-              <div className="h-48 w-full bg-white" />
             </div>
             <div className="relative py-32">
-              {/*<h1 className="text-white text-6xl font-bold">
+              {/*<h1 className="text-6xl font-bold text-white">
                 Where Supply meets demand.
               </h1>*/}
               {/* <button className="inline-block rounded-md border-2  border-[#cb202c] bg-[#cb202c] px-8 py-3 text-white hover:bg-[#cb202c]/65 font-bold transition-all">
@@ -82,13 +81,13 @@ export const StoreFrontHeader = () => {
           </div>
 
           <section
-            aria-labelledby="collection-heading"
-            className="relative -mt-96 sm:mt-0"
-          >
+  aria-labelledby="collection-heading"
+  className="relative mt-4 sm:mt-0"
+>
             <h2 id="collection-heading" className="sr-only">
               Collections
             </h2>
-            <div className="mx-auto grid max-w-md grid-cols-1 gap-6 px-4 sm:max-w-7xl sm:grid-cols-2 md:grid-cols-4 sm:px-6 lg:px-8">
+            <div className="grid max-w-md grid-cols-1 gap-6 px-4 mx-auto sm:max-w-7xl sm:grid-cols-2 md:grid-cols-4 sm:px-6 lg:px-8">
               {collections?.map((collection) => (
                <div
               key={collection.name}
